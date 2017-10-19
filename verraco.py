cb239318d05afe67495109676913cedf10202712
Víctor Cuervo <vcuervo@gmail.com>
2017-10-20 01:14:21 +0200

    Modificada cadena conexión MongoDB

--- verraco.py
+++ verraco.py
@@ -3,8 +3,15 @@
 from pymongo import MongoClient, errors
 import tools,os
 from random import randint
+from urllib import quote_plus
 
-client = MongoClient(os.environ.get('OPENSHIFT_MONGODB_DB_URL','mongodb://localhost:27017/'))
+user = os.environ.get('MONGODB_USER','victor')
+pwd = os.environ.get('MONGODB_PASSWORD','victor')
+bd = os.environ.get('MONGODB_DATABASE','localhost:27017/')
+
+uri = "mongodb://%s:%s@%s" % (quote_plus(user), quote_plus(pwd), quote_plus(bd))
+client = MongoClient(uri)
+
 db = client.verracos
 listado = db.listado
 
