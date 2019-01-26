cb770454cd2332a184e86847b19f90a986f76034
Víctor Cuervo <vcuervo@gmail.com>
2019-01-26 11:34:51 +0100

    Modificada versión Flask

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='Verracos',
-      version='1.0.2',
+      version='1.0.3',
       description='Aplicacion que permite localizar Verracos en Avila',
       author='Victor Cuervo',
       author_email='vcuervo@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask==0.10.1','pymongo>=3.2','tinys3>=0.1.11','Flask-Mail==0.9.1']
+      install_requires=['Flask>=0.12.3','pymongo>=3.2','tinys3>=0.1.11','Flask-Mail==0.9.1']
      )
